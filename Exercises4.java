23171e4630466e63beea89738faff63d9ecaa483
Amira Al-moalemi <110780774+AmiraAl-moalemi@users.noreply.github.com>
2022-08-08 11:45:42 +0300

    Update Exercises4.java

--- Exercises4.java
+++ Exercises4.java
@@ -1,7 +1,7 @@
 public class Exercises4 {
     public static void main(String[] args) {
         System.out.println((1000*5/100)+(1000));
-        System.out.println((1050*5/100])+(1050));
+        System.out.println((1050*5/100)+(1050));
         System.out.println((1102*5/100)+(1102));
         System.out.println("Balance with interest equal to 1157.625$");
     }
